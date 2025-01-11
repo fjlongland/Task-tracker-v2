a6466e5d4a63239f2b3a288bc77db49d7d41feac
fjlongland <fjlongland@gmail.com>
2025-01-11 14:28:13 +0200

    first commit

---
+++ src/frontend.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent = ..., flags = ...):
+        super().__init__(parent, flags)
+        x = 30
+        y = 40
+        width = 1000
+        height = 1000
+        self.setGeometry(x, y, width, height)
+        self.setWindowIcon(QIcon("imij.jpg"))
